cd707eb0068a234f06492ef27d4d6fe49ed7eb06
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-01-31 13:37:18 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,26 @@
+// console.log("Hello World");
+// console.log('Welcome to Angular Module...')
+// console.log(`
+//         Hopes So u All are enjoying This Session
+// `);
+// var a1,a2;
+// var a_1,a$1;
+//data types
+// number(int,short,double,float,long) , string(""/''), boolean(true/false)
+// any (number,string,boolean)
+// void (it is neutral data type => it does not point anything)
+// misec. null & undefined => It is used as data type as well as as value 
+var a = 10; // Forward declaration & defination
+// FileInputStream fis=null;
+// console.log("Value of a is "+a);
+var str;
+// console.log('Value of str is '+str);
+str = 'Sumit';
+// console.log('Value of str is '+str);
+var myany;
+myany = 2.5;
+console.log("value of myany is " + myany);
+myany = 'Sumit';
+console.log("value of myany is " + myany);
+myany = true;
+console.log("value of myany is " + myany);
